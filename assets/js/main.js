f82f33e39f79849058f43dbe3eca7494bcc02eea
axxmeddov <rovsen.brat@mail.ru>
2022-12-18 23:21:40 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.querySelector('.mag-div').addEventListener('click' , function () {
+    document.querySelector('.divv').classList.toggle('active')
+})  
+
+document.querySelector('.mag-div2').addEventListener('click' , function () {
+    document.querySelector('.divv1').classList.toggle('active')
+    
+})  
+
+document.querySelector('.mag-div3').addEventListener('click' , function () {
+    document.querySelector('.divv2').classList.toggle('active')
+})  
